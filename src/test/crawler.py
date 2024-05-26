88932739beb709b99551432c5a92cf126a0496b2
seunggyo9 <151025291+seunggyo9@users.noreply.github.com>
2024-05-26 11:21:09 +0900

    Update crawler.py

--- src/test/crawler.py
+++ src/test/crawler.py
@@ -1,16 +1,34 @@
-import unittest
-from mycrawler import fetch_page, parse_titles
+#크롤링시 필요한 라이브러리 불러오기
+from bs4 import BeautifulSoup
+import requests
+import re
+import datetime
+from tqdm import tqdm
+import sys
 
-class TestCrawler(unittest.TestCase):
-    
-    def test_fetch_page(self):
-        html = fetch_page('')
-        self.assertIsNotNone(html)
-    
-    def test_parse_titles(self):
-        html = "<html><body><h1>Title 1</h1><h1>Title 2</h1></body></html>"
-        titles = parse_titles(html)
-        self.assertEqual(titles, ['Title 1', 'Title 2'])
+# 페이지 url 형식에 맞게 바꾸어 주는 함수 만들기
+  #입력된 수를 1, 11, 21, 31 ...만들어 주는 함수
+def makePgNum(num):
+    if num == 1:
+        return num
+    elif num == 0:
+        return num+1
+    else:
+        return num+9*(num-1)
 
-if __name__ == '__main__':
-    unittest.main()
+# 크롤링할 url 생성하는 함수 만들기(검색어, 크롤링 시작 페이지, 크롤링 종료 페이지)
+
+def makeUrl(search, start_pg, end_pg):
+    if start_pg == end_pg:
+        start_page = makePgNum(start_pg)
+        url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=" + search + "&start=" + str(start_page)
+        print("생성url: ", url)
+        return url
+    else:
+        urls = []
+        for i in range(start_pg, end_pg + 1):
+            page = makePgNum(i)
+            url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=" + search + "&start=" + str(page)
+            urls.append(url)
+        print("생성url: ", urls)
+        return urls
