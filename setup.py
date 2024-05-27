9780e36ebde31661339297e8bc9fbbbc790eb683
seunggyo9 <151025291+seunggyo9@users.noreply.github.com>
2024-05-27 10:00:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+from os.path import basename, splitext
 from setuptools import find_packages, setup
 
 install_requires = [
@@ -8,11 +10,12 @@ install_requires = [
 
 setup(
     name='test',
-    version='1.0.0',
     packages=find_packages(where='src'),
+    version='1.0.0',
     package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     install_requires=install_requires,
     author='seunggyo',
     author_email='wxcv47@naver.com',
-    description='Naver News Crawler',
+    description='Desc',
 )
