cb4b99cff630fd79fd771eb827c92f4065fef414
seunggyo9 <151025291+seunggyo9@users.noreply.github.com>
2024-05-27 10:25:37 +0900

    Update crawler.py

--- src/test/crawler.py
+++ src/test/crawler.py
@@ -1,14 +1,2 @@
-def makeUrl(search, start_pg, end_pg):
-    if start_pg == end_pg:
-        start_page = makePgNum(start_pg)
-        url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=" + search + "&start=" + str(start_page)
-        print("생성url: ", url)
-        return url
-    else:
-        urls = []
-        for i in range(start_pg, end_pg + 1):
-            page = makePgNum(i)
-            url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=" + search + "&start=" + str(page)
-            urls.append(url)
-        print("생성url: ", urls)
-        return urls
+def myadd(a,b):
+    return a+b
