6b419fc3b02283af58a8d6d3cb601034957809a1
seunggyo9 <151025291+seunggyo9@users.noreply.github.com>
2024-05-27 10:16:45 +0900

    Update crawler.py

--- src/test/crawler.py
+++ src/test/crawler.py
@@ -1,15 +1,3 @@
-# 페이지 url 형식에 맞게 바꾸어 주는 함수 만들기
-  #입력된 수를 1, 11, 21, 31 ...만들어 주는 함수
-def makePgNum(num):
-    if num == 1:
-        return num
-    elif num == 0:
-        return num+1
-    else:
-        return num+9*(num-1)
-
-# 크롤링할 url 생성하는 함수 만들기(검색어, 크롤링 시작 페이지, 크롤링 종료 페이지)
-
 def makeUrl(search, start_pg, end_pg):
     if start_pg == end_pg:
         start_page = makePgNum(start_pg)
