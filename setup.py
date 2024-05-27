3f6bb9d0072ec0197ef5ce5de601989af92e60ce
seunggyo9 <151025291+seunggyo9@users.noreply.github.com>
2024-05-27 14:54:13 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
 from glob import glob
 from os.path import basename, splitext
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+import subprocess
 
 install_requires = [
     'pandas',
     'numpy',
     'nltk',
-    ]
+]
 
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        subprocess.call(['python', 'post_install.py'])
 
-
-setup (
-    name             = 'apple', 
+setup(
+    name='apple',
     packages=find_packages(where='src'),
-    version          = '1.0.0',
+    version='1.0.0',
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    install_requires = install_requires,
-    author           = 'seunggyo',
-    author_email     = 'wxcv47@naver.com',
-    description      = 'Desc'
+    install_requires=install_requires,
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    author='seunggyo',
+    author_email='wxcv47@naver.com',
+    description='Desc'
 )
